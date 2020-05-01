9d64f92b2cb9a74e5dede659df87756cdd4d7282
Lvless <64471300+Lvless@users.noreply.github.com>
2020-05-01 18:21:07 +0200

    Travail sur le script

---
+++ SCRIPTS/main.js
@@ -0,0 +1,13 @@
+// config visualisation
+
+const height = 200;
+const width = 600;
+const margin = {top : 20, right: 0, bottom: 0, left: 20};
+const color = 'steelblue';
+
+
+// Données CVS
+Const LStup = [ ]
+
+
+// Canevas SVG
