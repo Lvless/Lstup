50b6aab2a35265c8d005d7bdc4da74b029f8e425
Lvless <64471300+Lvless@users.noreply.github.com>
2020-05-12 17:50:39 +0200

    Présentation titres-en cours
    Travail sur la manière de présenter le titre. Pour le moment tout se superpose. WIP

--- scripts/main.js
+++ scripts/main.js
@@ -1,46 +1,109 @@
-
-//Sera le script principal quand j'aurai trouvé la bonne manière de le présenter
-
-// variables config pour la visualisation
-
-const height = 200;
-const width = 600;
-const margin = {top : 20, right: 0, bottom: 0, left: 20};
-const color = 'steelblue';
-
-
-// Données CVS
-
-const lstup = d3.csv("data/DATA_LSTUP09-19_TOP15.csv", function(d) {
-    return {
-        date: d.data,
-        substance: d.substance,
-        nombre_infr: parsefloat(d.valeur)
-    };
-  })
+//-------LOG:
+//Script ESSAI graphique SIMPLE
+// 06/5 : pas encore trouvé comment afficher graph avec mes données -> bug avec la partie données,
+// 07/5 :  ABANDON TRUC CIRCULAIRE POUR LE MOMENT : 
+//          ESSAI DE FAIRE UN BARPLOT SIMPLE ET QUE CA UTILISE MES DONNEES 
+//          -> TJ PAS REUSSI !! CA NE VEUT PAS UTILISER MON CSV... PK ?!
+// 08/5 :----> Ajouter [data].map a aidé pour utiliser les données, mais mnt faut les presenter juste
+// -------------------------
+// 
 
 
-// Canevas SVG
-const svg = d3.select('#maViz')
-            .append('svg')
-            .attr('width', width)
-            .attr('height', height)
-            .attr('style', 'font: 10x sans-serif')
+// Dimensions et marges du graph
+const margin = {top: 30, right: 30, bottom: 70, left: 60}; // marger
+const width = 460 - margin.left - margin.right; // largeur
+const height = 400 - margin.top - margin.bottom; // hauteur
+const color = '#a269b3'; // couleur violet
 
-const x = d3.scaleLinear()
-            .domain([0, d3.max(lstup, d=> d.count)])
 
-const y = d3.scaleBand()
-            .domain(lstup.map(d=> d.name))
-            .range([margin.top, height - margin.bottom])
-            
-svg.append('g')
-    .style('fill', color)
-    .selectAll('rect')
-    .data(lstup)
-    .enter()
-    .append('rect')
-    .attr('width', width)
-    .attr('height', 20)
-    .attr('x', 0)
-    .attr('y', 0)
+/////////////////////////////////////////////////////////
+
+// Canevas SVG + ajout dans le "body" de la page HTML
+const svg = d3.select("#maViz") // nom de ma visualisation sur index
+              .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+/////////////////////////////////////////////////////////
+
+// Données
+const data = d3.csv("data/DATA_LSTUP09V2.csv", function(data){
+//  return {
+//    substance: d.substance,
+//    valeur: parsefloat(d.valeur)};
+//}).then(function(data){
+/////////////////////////////////////////////////////////
+
+
+// ECHELLE X "-" : substances ---------------------------
+const x = d3.scaleBand() //échelle ordinnale à bandes
+            .domain([data].map(d => d.substance)) // données -> prendre substance en x, array donc [] 
+            .range([ 0, width ])
+            .padding(0.1) //marge entre les barres
+            .round(true)
+
+  // Placement texte sur axe x
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x)) //placement axe X ici avec methode "call"
+
+  
+
+
+
+///////////////////////////////////////////////////////
+
+// ECHELLE Y "|" : valeurs ---------------------------
+const y = d3.scaleLinear()
+            .domain([0, d3.max(data, d => d.valeur)])
+            .range([ height, 0 ]) // intervale de sortie de l'échelle
+
+  // placement valeurs sur axe y
+
+
+  //placement axe Y
+svg.append('g')  
+    .attr('tranform', `translate(0, ${margin.left}, 0)`)
+    .call(d3.axisLeft(y))
+
+
+/////////////////////////////////////////////////////
+
+	// Création de groupes SVG pour les barres et titres du graphique
+	Barres = svg.append('g');
+	Titres = svg.append('g')
+              .style('fill', 'black')
+              .attr("transform", "translate(-10,0)", "rotate(-45)")
+              .style("text-anchor", "end")
+
+
+// Barres--------------------------
+Barres.selectAll('rect')
+      .data(data)
+      .join('rect')
+      .attr('width', x.bandwidth())
+      .attr("height", d => y(d.valeur) - height )
+      .attr("x", d => x(d.substance))
+      .attr("y", d => y(d.valeur))
+      .attr("fill", color)
+
+	// Titres
+Titres.selectAll('text')
+	    .data(data)
+      .join('text')
+
+			.attr('dy', '0.35em')
+			.attr('x', d => x(d.substance))
+			.attr('y', d => y(d.valeur))
+			.text(d => d.substance);
+
+////////////////////////////////////////////////////
+
+})
+
+
+
+
+//LOAD ONClick BUTTON FUNCTION : en cours
---
+++ scripts/main_.js
@@ -0,0 +1,46 @@
+
+//Sera le script principal quand j'aurai trouvé la bonne manière de le présenter
+
+// variables config pour la visualisation
+
+const height = 200;
+const width = 600;
+const margin = {top : 20, right: 0, bottom: 0, left: 20};
+const color = 'steelblue';
+
+
+// Données CVS
+
+const lstup = d3.csv("data/DATA_LSTUP09-19_TOP15.csv", function(d) {
+    return {
+        date: d.data,
+        substance: d.substance,
+        nombre_infr: parsefloat(d.valeur)
+    };
+  })
+
+
+// Canevas SVG
+const svg = d3.select('#maViz')
+            .append('svg')
+            .attr('width', width)
+            .attr('height', height)
+            .attr('style', 'font: 10x sans-serif')
+
+const x = d3.scaleLinear()
+            .domain([0, d3.max(lstup, d=> d.count)])
+
+const y = d3.scaleBand()
+            .domain(lstup.map(d=> d.name))
+            .range([margin.top, height - margin.bottom])
+            
+svg.append('g')
+    .style('fill', color)
+    .selectAll('rect')
+    .data(lstup)
+    .enter()
+    .append('rect')
+    .attr('width', width)
+    .attr('height', 20)
+    .attr('x', 0)
+    .attr('y', 0)
